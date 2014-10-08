cfa08b4d3ba890512e4497747be571f6756c3817
laxmikant4644 <laxmikant.4644@gmail.com>
2014-10-09 00:25:17 +0530

    search for student from database

---
+++ Public/php/student_search.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * MYSQL Cerendentials Include Page
+ *
+ * @author Laxmikant Revdikar  <laxmikant.4644@gmail.com>
+ */
+require_once("connect.php");
+	if(isset($_POST['name'])){
+		$name=strip_tags($_POST['name']);
+	    $result=mysql_fetch_assoc(mysqli_query($conn,"SELECT `name` FROM student WHERE `name` like '%{$name}%' ORDER BY `name`"));
+		if($query){
+			foreach ($name as $result) {
+				echo '{$name}</br>';
+			}
+		}
+	}
+?>
