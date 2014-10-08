2dfdcb361086aac8e85b89bf115d5ca85f7ff102
laxmikant4644 <laxmikant.4644@gmail.com>
2014-10-09 00:31:03 +0530

    auto_search for faculty

---
+++ Public/php/faculty_autosearch.php
@@ -0,0 +1,20 @@
+<?php
+	/**
+ * MYSQL Cerendentials Include Page
+ *
+ * @author Laxmikant Revdikar  <laxmikant.4644@gmail.com>
+ */
+
+require_once("connect.php");
+	if(isset($_REQUEST['name'])){
+		$name=strip_tags($_REQUEST['name']);
+		$query=mysql_query("SELECT `name` FROM faculty WHERE `name` like '%{$name}%'");
+		$mk="";
+		if($query){
+			while($result=mysql_fetch_array($query)) {
+				$mk=$mk."<li><a href='../public/php/faculty_profile.php?name={$result['name']}'>{$result['name']}</a></li>";
+			}
+		}
+		echo $mk;
+	}
+?>
