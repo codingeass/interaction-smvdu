4cc257af0d8b83679e8ecad52ac43043c7cd2494
codingeass <amandeeptheviper@gmail.com>
2014-11-03 22:49:25 +0530

    trip space from email in register

--- Public/php/facreg.php
+++ Public/php/facreg.php
@@ -18,16 +18,16 @@ if( isset($_REQUEST["name"]) && isset($_REQUEST["email"]) && isset($_REQUEST["pa
 	require_once("connect.php");
      // if using above php 5.5 then try to use password_hash than md5 as its better than md5
 	//**check if email is already registered
-	$query="Select * from user where email='".urldecode(strip_tags($_REQUEST["email"]))."';";
+	$query="Select * from user where email='".trim(urldecode(strip_tags($_REQUEST["email"])))."';";
 	$result=mysql_query($query);
 	while($res=mysql_fetch_array($result))
 		exit ('Email is already registered');
 
-	$query="Insert into user (name,email,password,type)values('".strtolower(urldecode(strip_tags($_REQUEST["name"])))."','".urldecode(strip_tags($_REQUEST["email"]))."','".md5(md5(urldecode(strip_tags($_REQUEST["pass"]))))."','".urldecode(strip_tags($_REQUEST["type"]))."');";
+	$query="Insert into user (name,email,password,type)values('".trim(strtolower(urldecode(strip_tags($_REQUEST["name"]))))."','".trim(urldecode(strip_tags($_REQUEST["email"])))."','".md5(md5(urldecode(strip_tags($_REQUEST["pass"]))))."','".urldecode(strip_tags($_REQUEST["type"]))."');";
 	$result=mysql_query($query)
                     or die("Invalid Entry");
 
-    $quert="Select * from user where email='".urldecode(strip_tags($_REQUEST["email"]))."';";
+    $quert="Select * from user where email='".trim(urldecode(strip_tags($_REQUEST["email"])))."';";
 	$resulty=mysql_query($quert)
 				or die("Invalid Entry");
 	while($res=mysql_fetch_array($resulty))
@@ -37,7 +37,7 @@ if( isset($_REQUEST["name"]) && isset($_REQUEST["email"]) && isset($_REQUEST["pa
                     or die("Invalid Entry");
       echo "You are registered successfully";
       //check for parameter of exit when change login page
-	$myfile = fopen("../xml/blog/".urldecode(strip_tags($_REQUEST["email"])).".xml", "w") or die("Unable to open file!");
+	$myfile = fopen("../xml/blog/".trim(urldecode(strip_tags($_REQUEST["email"]))).".xml", "w") or die("Unable to open file!");
 	$txt = "<?xml version='1.0' encoding='utf-8' standalone='no'?>
 	<!DOCTYPE chatting [
 	<!ELEMENT Complete (BlogSection , title , author , content , comments , comment)>
--- Public/php/stureg.php
+++ Public/php/stureg.php
@@ -16,28 +16,28 @@ if( isset($_REQUEST["name"]) && isset($_REQUEST["email"]) && isset($_REQUEST["pa
 	require_once("connect.php");
      // if using above php 5.5 then try to use password_hash than md5 as its better than md5
 	//**check if email is already registered
-	$query="Select * from user where email='".urldecode(strip_tags($_REQUEST["email"]))."';";
+	$query="Select * from user where email='".trim(urldecode(strip_tags($_REQUEST["email"])))."';";
 	$result=mysql_query($query);
 	while($res=mysql_fetch_array($result))
 	{
 		exit ('Email is already registered');		
 	}
-
-	$query="Insert into user (name,email,password,type)values('".urldecode(strtolower(strip_tags($_REQUEST["name"])))."','".urldecode(strip_tags($_REQUEST["email"]))."','".md5(md5(urldecode(strip_tags($_REQUEST["pass"]))))."','".urldecode(strip_tags($_REQUEST["type"]))."');";
+	//trim space from each of the element from java
+	$query="Insert into user (name,email,password,type)values('".trim(urldecode(strtolower(strip_tags($_REQUEST["name"]))))."','".trim(urldecode(strip_tags($_REQUEST["email"])))."','".md5(md5(urldecode(strip_tags($_REQUEST["pass"]))))."','".urldecode(strip_tags($_REQUEST["type"]))."');";
 	$result=mysql_query($query)
                     or die('Invalid Entry');
 
-    $quert="Select * from user where email='".urldecode(strip_tags($_REQUEST["email"]))."';";
+    $quert="Select * from user where email='".trim(urldecode(strip_tags($_REQUEST["email"])))."';";
 	$resulty=mysql_query($quert)
 				or die('Invalid Entry');
 	while($res=mysql_fetch_array($resulty))
 	{                
-    $quer="Insert into student (id,name,sex,entry_no,branch,semester,date_of_birth,date_of_joining,address,contact,project,area_of_interest)values('".$res["id"]."','".urldecode(strip_tags($_REQUEST["name"]))."','".strip_tags($_REQUEST["sex"])."','".urldecode(strip_tags($_REQUEST["ent"]))."','".urldecode(strip_tags($_REQUEST["bran"]))."','".urldecode(strip_tags($_REQUEST["sem"]))."','".urldecode(strip_tags($_REQUEST["bir"]))."','".urldecode(strip_tags($_REQUEST["join"]))."','".urldecode(strip_tags($_REQUEST["add"]))."','".urldecode(strip_tags($_REQUEST["cont"]))."','".urldecode(strip_tags($_REQUEST["pro"]))."','".urldecode(strip_tags($_REQUEST["are"]))."');";
+    $quer="Insert into student (id,name,sex,entry_no,branch,semester,date_of_birth,date_of_joining,address,contact,project,area_of_interest)values('".$res["id"]."','".trim(urldecode(strip_tags($_REQUEST["name"])))."','".strip_tags($_REQUEST["sex"])."','".urldecode(strip_tags($_REQUEST["ent"]))."','".urldecode(strip_tags($_REQUEST["bran"]))."','".urldecode(strip_tags($_REQUEST["sem"]))."','".urldecode(strip_tags($_REQUEST["bir"]))."','".urldecode(strip_tags($_REQUEST["join"]))."','".urldecode(strip_tags($_REQUEST["add"]))."','".urldecode(strip_tags($_REQUEST["cont"]))."','".urldecode(strip_tags($_REQUEST["pro"]))."','".urldecode(strip_tags($_REQUEST["are"]))."');";
 	$resul=mysql_query($quer)
                     or die('Invalid Entry');
       echo 'You are registered successfully';
       //check for parameter of exit when change login page
-    $myfile = fopen("../xml/blog/".urldecode(strip_tags($_REQUEST["email"])).".xml", "w") or die("Unable to open file!");
+    $myfile = fopen("../xml/blog/".trim(urldecode(strip_tags($_REQUEST["email"]))).".xml", "w") or die("Unable to open file!");
 	$txt = "<?xml version='1.0' encoding='utf-8' standalone='no'?>
 	<!DOCTYPE chatting [
 	<!ELEMENT Complete (BlogSection , title , author , content , comments , comment)>
