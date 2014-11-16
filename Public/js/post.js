75fd8f919e75dd3a59e9ea50cc1fc9857e484231
codingeass <amandeeptheviper@gmail.com>
2014-11-16 17:17:19 +0530

    added display- some part of content in blog list

--- Public/js/post.js
+++ Public/js/post.js
@@ -14,10 +14,37 @@ function listpost(){
 
 		x=xmlDoc.getElementsByTagName("BlogSection");
 		var mk="";
-		for (i=0;i<x.length;i++)
+		var cont="";
+		var mlink="";
+		for (i=x.length-1;i>-1;i--)
 		{
-			mk=mk+"<section style='border:1px solid black;border-radius:3px;padding:15px;margin:50px;'><h2><a href='blog.html?t="+x[i].getElementsByTagName("time")[0].childNodes[0].nodeValue+"&em="+x[i].getElementsByTagName("email")[0].childNodes[0].nodeValue+"' target='_blank'>"+x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue+"</a></h2>";
-			mk=mk+"<div style='right:23px;font-size:20px;'>Author:"+x[i].getElementsByTagName("author")[0].childNodes[0].nodeValue+"</div></section>"
+			mlink="<a href='blog.html?t="+x[i].getElementsByTagName("time")[0].childNodes[0].nodeValue+"&em="+x[i].getElementsByTagName("email")[0].childNodes[0].nodeValue+"' target='_blank'>";
+			mk=mk+"<section style='border:1px solid #d0ced4;border-radius:3px;padding:15px;margin:50px;margin-bottom:0px;background-color:#f1eef7;'><h2>"+mlink+x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue+"</a></h2><hr>";
+			mk=mk+"<div style='right:23px;font-size:17px;'>Author:"+x[i].getElementsByTagName("author")[0].childNodes[0].nodeValue+"</div>";
+			{
+				 if (window.XMLHttpRequest)
+				  {// code for IE7+, Firefox, Chrome, Opera, Safari
+				  xmlhttp1=new XMLHttpRequest();
+				  }
+				 else
+				  {// code for IE6, IE5
+				  xmlhttp1=new ActiveXObject("Microsoft.XMLHTTP");
+				  }
+				 xmlhttp1.open("GET","public\\xml\\blog\\"+x[i].getElementsByTagName("email")[0].childNodes[0].nodeValue+".xml?"+Math.random(),false);
+				 xmlhttp1.send();
+				 xmlDoc1=xmlhttp1.responseXML; 
+				 x1=xmlDoc1.getElementsByTagName("BlogSection");
+				 cont="";
+				 for (i1=x1.length-1;i1>-1;i1--)
+				 {
+				 	if(x1[i1].getElementsByTagName("time")[0].childNodes[0].nodeValue==x[i].getElementsByTagName("time")[0].childNodes[0].nodeValue)
+				 	{
+				 		cont=x1[i1].getElementsByTagName("content")[0].childNodes[0].nodeValue.substring(0,300);
+				 		break;
+				 	}
+				 }
+			}
+			mk=mk+"<div style='right:23px;font-size:18px;'>"+cont+"<b>&nbsp;&nbsp;&nbsp;"+mlink+"Continue...</a></b></div></section>";
 		}
 		mg.innerHTML=mk;
 		// for (i=0;i<x.length;i++)
